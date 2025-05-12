d3b8600bf484bc4dc31effd984a48536e477118d
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2025-05-12 19:14:53 +0530

    resolved merge conflict

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAccount } from '../context/AccountProvider';
-
-const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { account } = useAccount();
-  if (!account) {
-    return <Navigate to="/" replace />;
-  }
-  return children;
-};
-
-export default ProtectedRoute;
-=======
 import { ReactNode } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useUser } from '../context/UserContext';
@@ -34,4 +19,3 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
 };
 
 export default ProtectedRoute;
->>>>>>> ef9de5f (added most of the frontend part)
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-=======
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -17,4 +8,3 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
 });
->>>>>>> ef9de5f (added most of the frontend part)
